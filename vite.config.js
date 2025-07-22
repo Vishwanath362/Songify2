ddb4acf08f29be5c0686ab6240e99725df2dc256
Vishwanath362 <156990926+Vishwanath362@users.noreply.github.com>
2025-07-22 12:27:04 +0530

    base

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-   proxy: {
-      '/api': 'http://localhost:3000',
+  plugins: [react(), tailwindcss()],
+  base: './', // ✅ Fix for Netlify/relative URLs
+
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000', // ✅ Only used during local dev
     },
-  
+  },
 })
